d387193e590990463de858bd6d70676067d25d2a
Harsh Shandilya <msfjarvis@gmail.com>
2017-03-06 00:45:25 +0530

    MainActivity : Remove a hardcode
    Change-Id: I2b81021121e11993385b58effa7ce7114d516253

--- app/src/main/java/com/ota/updates/activities/MainActivity.java
+++ app/src/main/java/com/ota/updates/activities/MainActivity.java
@@ -69,7 +69,7 @@ public class MainActivity extends Activity implements Constants{
 	private Builder mPlayStoreDialog;
 
 	private boolean isLollipop;
-	
+
 	public static ProgressBar mProgressBar;
 
 	private BroadcastReceiver mReceiver = new BroadcastReceiver() {
@@ -100,20 +100,20 @@ public class MainActivity extends Activity implements Constants{
 		setActionBar(toolbar);
 		toolbar.setTitle(getResources().getString(R.string.app_name));
 
-		boolean firstRun = Preferences.getFirstRun(mContext);				
+		boolean firstRun = Preferences.getFirstRun(mContext);
 		if(firstRun) {
 			Preferences.setFirstRun(mContext, false);
 			showWhatsNew();
 		}
-		
+
 		String oldChangelog = Preferences.getOldChangelog(mContext);
 		String currentChangelog = getResources().getString(R.string.app_version);
 		if(!oldChangelog.equals(currentChangelog)) {
 			showWhatsNew();
 		}
-		
+
 		// Create download directories if needed
-		File installAfterFlashDir = new File(SD_CARD 
+		File installAfterFlashDir = new File(SD_CARD
 				+ File.separator
 				+ OTA_DOWNLOAD_DIR
 				+ File.separator
@@ -199,11 +199,11 @@ public class MainActivity extends Activity implements Constants{
 				MainActivity.this.finish();
 			}
 		});
-		
+
 		// Donate Dialog
 		mDonateDialog = new AlertDialog.Builder(this);
 		String[] donateItems = { "PayPal", "BitCoin" };
-		mDonateDialog.setTitle(getResources().getString(R.string.donate))		
+		mDonateDialog.setTitle(getResources().getString(R.string.donate))
 		.setSingleChoiceItems(donateItems, 0, null)
 		.setPositiveButton(getResources().getString(R.string.ok), new DialogInterface.OnClickListener() {
 
@@ -218,13 +218,13 @@ public class MainActivity extends Activity implements Constants{
 				}
 				Intent intent = new Intent(Intent.ACTION_VIEW);
 				intent.setData(Uri.parse(url));
-				
+
 				try {
 					startActivity(intent);
 				} catch(ActivityNotFoundException ex) {
 					// Nothing to handle BitCoin payments. Send to Play Store
 					if (DEBUGGING)
-						Log.d(TAG, ex.getMessage());					
+						Log.d(TAG, ex.getMessage());
 					mPlayStoreDialog.show();
 				}
 			}
@@ -236,7 +236,7 @@ public class MainActivity extends Activity implements Constants{
 				dialog.cancel();
 			}
 		});
-		
+
 		mPlayStoreDialog = new AlertDialog.Builder(mContext);
 		mPlayStoreDialog.setCancelable(true);
 		mPlayStoreDialog.setTitle(R.string.main_playstore_title);
@@ -270,7 +270,7 @@ public class MainActivity extends Activity implements Constants{
 
 		TextView updateAvailableSummary = (TextView) findViewById(R.id.main_tv_update_available_summary);
 		TextView updateNotAvailableSummary = (TextView) findViewById(R.id.main_tv_no_update_available_summary);
-		
+
 		mProgressBar = (ProgressBar) findViewById(R.id.bar_main_progress_bar);
 		mProgressBar.setVisibility(View.GONE);
 
@@ -357,11 +357,11 @@ public class MainActivity extends Activity implements Constants{
 			updateNotAvailableSummary.setText(lastChecked + " " + time);
 		}
 	}
-	
+
 	private void updateAddonsLayout() {
 		CardView addonsLink = (CardView) findViewById(R.id.layout_main_addons);
 		addonsLink.setVisibility(View.GONE);
-		
+
 		if (RomUpdate.getAddonsCount(mContext) > 0) {
 			addonsLink.setVisibility(View.VISIBLE);
 		}
@@ -370,7 +370,7 @@ public class MainActivity extends Activity implements Constants{
 	private void updateDonateLinkLayout() {
 		CardView donateLink = (CardView) findViewById(R.id.layout_main_dev_donate_link);
 		donateLink.setVisibility(View.GONE);
-		
+
 		if (!(RomUpdate.getDonateLink(mContext).trim().equals("null")) 
 				|| !(RomUpdate.getBitCoinLink(mContext).trim().equals("null"))) {
 			donateLink.setVisibility(View.VISIBLE);
@@ -442,14 +442,14 @@ public class MainActivity extends Activity implements Constants{
 		Intent intent = new Intent(mContext, AvailableActivity.class);
 		startActivity(intent);
 	}
-	
+
 	public void openAddons(View v) {
 		Intent intent = new Intent(mContext, AddonActivity.class);
 		startActivity(intent);
 	}
 
 	public void openDonationPage(View v) {
-		
+
 		boolean payPalLinkAvailable = RomUpdate.getDonateLink(mContext).trim().equals("null");
 		boolean bitCoinLinkAvailable = RomUpdate.getBitCoinLink(mContext).trim().equals("null");
 		if (!payPalLinkAvailable && !bitCoinLinkAvailable) {
@@ -458,7 +458,7 @@ public class MainActivity extends Activity implements Constants{
 			String url = RomUpdate.getDonateLink(mContext);
 			Intent intent = new Intent(Intent.ACTION_VIEW);
 			intent.setData(Uri.parse(url));
-			startActivity(intent);			
+			startActivity(intent);
 		} else {
 			String url = RomUpdate.getBitCoinLink(mContext);
 			Intent intent = new Intent(Intent.ACTION_VIEW);
@@ -478,7 +478,7 @@ public class MainActivity extends Activity implements Constants{
 		Intent intent = new Intent(mContext, SettingsActivity.class);
 		startActivity(intent);
 	}
-	
+
 	public void openChangelog (View v) {
 		String title = getResources().getString(R.string.changelog);
 		String changelog = RomUpdate.getChangelog(mContext);
@@ -490,7 +490,7 @@ public class MainActivity extends Activity implements Constants{
 		String changelog = getResources().getString(R.string.changelog_url);
 		new Changelog(this, mContext, title, changelog, true).execute();
 	}
-	
+
 	public static void updateProgress(int progress) {
 		if(mProgressBar != null) {
 			mProgressBar.setProgress(progress);
@@ -506,7 +506,7 @@ public class MainActivity extends Activity implements Constants{
 
 		CompatibilityTask(Context context) {
 			mContext = context;
-			mPropName = mContext.getResources().getString(R.string.prop_name);
+			mPropName = Constants.OTA_MANIFEST;
 		}
 
 		@Override
