halogenOS/android_packages_apps_OTAUpdates

commit d1f350e04d28c36d979d436d97c934909a0772f7

OTAUpdates : Chem lab rebase
* Has to be the biggest Java commit to this app ever

What's changed?

-> Missing copyright headers added
-> Fixed most of the *legit* lint warnings and removed a lot of dead code
-> Optimize imports everywhere
-> Plug nearly all memory leaks (most caused by placing Context classes in static fields)
-> Wipe out all RomHut promotion stuff, since we don't have any collaboration with them
-> Remove Bitcoin donate link handling, since we've got a team PayPal for donations
-> Completely remove all Html.toHtml calls (FUCK YOU MATTHEW!)
-> Robust handling of cases where root is not available (Ideally, we should never need root)

--> Bumped version number to 4.0.0

TODO : I've had to comment out many AvailableActivity#updateProgress and MainActivity#updateProgress
calls due to issues with calling them from static contexts, I DO NOT know what regressions we are
introducing here, so this needs to be tested thoroughly

Change-Id: Ica6293931b5ee582e31c2671a4b09b91ad028580

app/src/main/java/com/ota/updates/activities/MainActivity.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2015 Matt Booth (Kryten2k35).
+ * Copyright (C) 2017 The halogenOS Project.
  *
  * Licensed under the Attribution-NonCommercial-ShareAlike 4.0 International
  * (the "License") you may not use this file except in compliance with the License.
@@ -16,8 +17,6 @@
 
 package com.ota.updates.activities;
 
-import android.app.ActionBar;
-import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
@@ -32,11 +31,8 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
-import android.text.Html;
 import android.text.format.DateFormat;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -44,7 +40,6 @@ import android.view.WindowManager;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toolbar;
-
 import com.ota.updates.R;
 import com.ota.updates.RomUpdate;
 import com.ota.updates.tasks.Changelog;
@@ -62,13 +57,13 @@ public class MainActivity extends Activity implements Constants{
 
     public final String TAG = this.getClass().getSimpleName();
 
-    private static Context mContext;
+    private Context mContext;
 
     private Builder mCompatibilityDialog;
     private Builder mDonateDialog;
     private Builder mPlayStoreDialog;
 
-    public static ProgressBar mProgressBar;
+    public ProgressBar mProgressBar;
 
     private BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
@@ -90,8 +85,6 @@ public class MainActivity extends Activity implements Constants{
     public void onCreate(Bundle savedInstanceState) {
 
         mContext = this;
-        setTheme(Preferences.getTheme(mContext));
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ota_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
@@ -133,6 +126,7 @@ public class MainActivity extends Activity implements Constants{
                     ((Activity) mContext).finish();
                 }
             })
+            .setCancelable(false)
             .show();
         } else {
             new CompatibilityTask(mContext).execute();
@@ -195,7 +189,7 @@ public class MainActivity extends Activity implements Constants{
 
         // Donate Dialog
         mDonateDialog = new AlertDialog.Builder(this);
-        String[] donateItems = { "PayPal", "BitCoin" };
+        String[] donateItems = { "PayPal" };
         mDonateDialog.setTitle(getResources().getString(R.string.donate))
         .setSingleChoiceItems(donateItems, 0, null)
         .setPositiveButton(getResources().getString(R.string.ok), new DialogInterface.OnClickListener() {
@@ -203,14 +197,20 @@ public class MainActivity extends Activity implements Constants{
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 String url;
+                Intent intent = new Intent(Intent.ACTION_VIEW);
                 int selectedPosition = ((AlertDialog)dialog).getListView().getCheckedItemPosition();
                 if (selectedPosition == 0) {
                     url = RomUpdate.getDonateLink(mContext);
-                } else {
-                    url = RomUpdate.getBitCoinLink(mContext);
+                    if (url != null){
+                        final Uri uri = Uri.parse(url);
+                        if (uri.getScheme() == null){
+                            url = "http://" + url;
+                        }
+                    } else {
+                        return;
+                    }
+                    intent.setData(Uri.parse(url));
                 }
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse(url));
 
                 try {
                     startActivity(intent);
@@ -275,35 +275,21 @@ public class MainActivity extends Activity implements Constants{
 
             if (Preferences.getDownloadFinished(mContext)) { //  Update already finished?
                 updateAvailableTitle.setText(getResources().getString(R.string.main_update_finished));
-                String htmlColorOpen = "<font color='#33b5e5'>";
-                String htmlColorClose = "</font>";
                 String updateSummary = RomUpdate.getVersionName(mContext)
-                        + "<br />"
-                        + htmlColorOpen
-                        + getResources().getString(R.string.main_download_completed_details)
-                        + htmlColorClose;
-                updateAvailableSummary.setText(Html.fromHtml(updateSummary));
+                        + "\n\n"
+                        + getResources().getString(R.string.main_download_completed_details);
+                updateAvailableSummary.setText(updateSummary);
             } else if (Preferences.getIsDownloadOnGoing(mContext)) {
                 updateAvailableTitle.setText(getResources().getString(R.string.main_update_progress));
                 mProgressBar.setVisibility(View.VISIBLE);
-                String htmlColorOpen;
-                htmlColorOpen = "<font color='#33b5e5'>";
-                String htmlColorClose = "</font>";
-                String updateSummary = htmlColorOpen
-                        + getResources().getString(R.string.main_tap_to_view_progress)
-                        + htmlColorClose;
-                updateAvailableSummary.setText(Html.fromHtml(updateSummary));
+                String updateSummary = getResources().getString(R.string.main_tap_to_view_progress);
+                updateAvailableSummary.setText(updateSummary);
             } else {
                 updateAvailableTitle.setText(getResources().getString(R.string.main_update_available));
-                String htmlColorOpen;
-                htmlColorOpen = "<font color='#33b5e5'>";
-                String htmlColorClose = "</font>";
                 String updateSummary = RomUpdate.getVersionName(mContext)
-                        + "<br />"
-                        + htmlColorOpen
-                        + getResources().getString(R.string.main_tap_to_download)
-                        + htmlColorClose;
-                updateAvailableSummary.setText(Html.fromHtml(updateSummary));
+                        + "\n\n"
+                        + getResources().getString(R.string.main_tap_to_download);
+                updateAvailableSummary.setText(updateSummary);
 
             }
         } else {
@@ -339,8 +325,7 @@ public class MainActivity extends Activity implements Constants{
         CardView donateLink = (CardView) findViewById(R.id.layout_main_dev_donate_link);
         donateLink.setVisibility(View.GONE);
 
-        if (!(RomUpdate.getDonateLink(mContext).trim().equals("null"))
-                || !(RomUpdate.getBitCoinLink(mContext).trim().equals("null"))) {
+        if (!(RomUpdate.getDonateLink(mContext).trim().equals("null"))) {
             donateLink.setVisibility(View.VISIBLE);
         }
     }
@@ -355,42 +340,33 @@ public class MainActivity extends Activity implements Constants{
     }
 
     private void updateRomInformation() {
-        String htmlColorOpen;
-        htmlColorOpen = "<font color='#33b5e5'>";
-        String htmlColorClose = "</font>";
 
         //ROM name
         TextView romName = (TextView) findViewById(R.id.tv_main_rom_name);
-        String romNameTitle = getApplicationContext().getResources().getString(R.string.main_rom_name) + " ";
         String romNameActual = Utils.getProp(OTA_ROMNAME);
-        romName.setText(Html.fromHtml(romNameTitle + htmlColorOpen + romNameActual + htmlColorClose));
+        romName.setText(String.format("%s%s", romName.getText(), romNameActual));
 
         //ROM version
         TextView romVersion = (TextView) findViewById(R.id.tv_main_rom_version);
-        String romVersionTitle = getApplicationContext().getResources().getString(R.string.main_rom_version) + " ";
         String romVersionActual = Utils.getProp(OTA_VERSION);
-        romVersion.setText(Html.fromHtml(romVersionTitle + htmlColorOpen + romVersionActual + htmlColorClose));
+        romVersion.setText(String.format("%s%s",romVersion.getText(),romVersionActual));
 
         //ROM date
         TextView romDate = (TextView) findViewById(R.id.tv_main_rom_date);
-        String romDateTitle = getApplicationContext().getResources().getString(R.string.main_rom_build_date) + " ";
         String romDateActual = Utils.getProp("ro.build.date");
-        romDate.setText(Html.fromHtml(romDateTitle + htmlColorOpen + romDateActual + htmlColorClose));
+        romDate.setText(String.format("%s%s", romDate.getText(), romDateActual));
 
         //ROM android version
         TextView romAndroid = (TextView) findViewById(R.id.tv_main_android_version);
-        String romAndroidTitle = getApplicationContext().getResources().getString(R.string.main_android_verison) + " ";
         String romAndroidActual = Utils.getProp("ro.build.version.release");
-        romAndroid.setText(Html.fromHtml(romAndroidTitle + htmlColorOpen + romAndroidActual + htmlColorClose));
+        romAndroid.setText(String.format("%s%s",romAndroid.getText(), romAndroidActual));
 
         //ROM developer
         TextView romDeveloper = (TextView) findViewById(R.id.tv_main_rom_developer);
         boolean showDevName = !RomUpdate.getDeveloper(this).equals("null");
         romDeveloper.setVisibility(showDevName? View.VISIBLE : View.GONE);
-
-        String romDeveloperTitle = getApplicationContext().getResources().getString(R.string.main_rom_developer) + " ";
         String romDeveloperActual = RomUpdate.getDeveloper(this);
-        romDeveloper.setText(Html.fromHtml(romDeveloperTitle + htmlColorOpen + romDeveloperActual + htmlColorClose));
+        romDeveloper.setText(String.format("%s%s",romDeveloper.getText(),romDeveloperActual));
 
     }
 
@@ -411,24 +387,10 @@ public class MainActivity extends Activity implements Constants{
     public void openDonationPage(View v) {
 
         boolean payPalLinkAvailable = RomUpdate.getDonateLink(mContext).trim().equals("null");
-        boolean bitCoinLinkAvailable = RomUpdate.getBitCoinLink(mContext).trim().equals("null");
-        if (!payPalLinkAvailable && !bitCoinLinkAvailable) {
+        if (!payPalLinkAvailable) {
             mDonateDialog.show();
         } else if (payPalLinkAvailable) {
             String url = RomUpdate.getDonateLink(mContext);
-            if (url != null){
-                final Uri uri = Uri.parse(url);
-                if (uri.getScheme() == null){
-                    url = "http://" + url;
-                }
-            } else {
-                return;
-            }
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setData(Uri.parse(url));
-            startActivity(intent);
-        } else {
-            String url = RomUpdate.getBitCoinLink(mContext);
             if (url != null){
                 final Uri uri = Uri.parse(url);
                 if (uri.getScheme() == null){
@@ -475,7 +437,7 @@ public class MainActivity extends Activity implements Constants{
         new Changelog(this, mContext, title, changelog, true).execute();
     }
 
-    public static void updateProgress(int progress) {
+    public void updateProgress(int progress) {
         if(mProgressBar != null) {
             mProgressBar.setProgress(progress);
         }

app/src/main/AndroidManifest.xml
@@ -17,7 +17,7 @@
 <manifest xmlns:android="http://schemas.android.com/apk/res/android"
     xmlns:tools="http://schemas.android.com/tools"
     package="com.ota.updates"
-    android:versionCode="19"
+    android:versionCode="20"
     android:versionName="@string/app_version"
     android:sharedUserId="android.uid.system" >
 

app/src/main/java/com/ota/updates/OtaUpdates.java
@@ -1,10 +1,27 @@
-package com.ota.updates;
+/*
+ * Copyright (C) 2015 Matt Booth (Kryten2k35).
+ * Copyright (C) 2017 The halogenOS Project.
+ *
+ * Licensed under the Attribution-NonCommercial-ShareAlike 4.0 International
+ * (the "License") you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import java.util.Set;
+package com.ota.updates;
 
 import android.app.Application;
 import android.support.v4.util.ArrayMap;
 import android.util.Log;
+
+import java.util.Set;
 
 public class OtaUpdates extends Application {
     private static ArrayMap<Integer, Long> mAddonsDownloads = new ArrayMap<Integer, Long>();
@@ -16,7 +33,7 @@ public class OtaUpdates extends Application {
 
     public static long getAddonDownload(int key) {
         Log.d("OtaUpdates", "Getting Addon with Key: " + key);
-        return (Long) mAddonsDownloads.get(key);
+        return mAddonsDownloads.get(key);
     }
 
     public static void removeAddonDownload(int key) {

app/src/main/java/com/ota/updates/RomUpdate.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2015 Matt Booth (Kryten2k35).
+ * Copyright (C) 2017 The halogenOS Project.
  *
  * Licensed under the Attribution-NonCommercial-ShareAlike 4.0 International
  * (the "License") you may not use this file except in compliance with the License.
@@ -14,15 +15,16 @@
  * limitations under the License.
  */
 
+
 package com.ota.updates;
 
-
-import java.io.File;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 
 import com.ota.updates.utils.Constants;
+
+import java.io.File;
 
 public class RomUpdate implements Constants {
 
@@ -30,18 +32,15 @@ public class RomUpdate implements Constants {
 
     private static final String PREF_NAME = "ROMUpdate";
 
-    private static String NAME = "rom_name";
     private static String VERSION_NAME = "rom_version_name";
     private static String VERSION_NUMBER = "rom_version_number";
     private static String DIRECT_URL = "rom_direct_url";
     private static String HTTP_URL = "rom_http_url";
     private static String MD5 = "rom_md5";
     private static String CHANGELOG = "rom_changelog";
-    private static String ANDROID = "rom_android_ver";
     private static String WEBSITE = "rom_website";
     private static String DEVELOPER = "rom_developer";
     private static String DONATE_LINK = "rom_donate_link";
-    private static String BTC_LINK = "rom_bitcoin_link";
     private static String FILESIZE = "rom_filesize";
     private static String AVAILABILITY = "update_availability";
     private static String URL_DOMAIN = "rom_url_domain";
@@ -55,10 +54,6 @@ public class RomUpdate implements Constants {
 
     private static SharedPreferences getPrefs(Context context) {
         return context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
-    }
-
-    public static String getRomName(Context context) {
-        return getPrefs(context).getString(NAME, DEF_VALUE);
     }
 
     public static String getVersionName(Context context) {
@@ -85,10 +80,6 @@ public class RomUpdate implements Constants {
         return getPrefs(context).getString(CHANGELOG, DEF_VALUE);
     }
 
-    public static String getAndroidVersion(Context context) {
-        return getPrefs(context).getString(ANDROID, DEF_VALUE);
-    }
-
     public static String getWebsite(Context context) {
         return getPrefs(context).getString(WEBSITE, DEF_VALUE);
     }
@@ -99,10 +90,6 @@ public class RomUpdate implements Constants {
 
     public static String getDonateLink(Context context) {
         return getPrefs(context).getString(DONATE_LINK, DEF_VALUE);
-    }
-
-    public static String getBitCoinLink(Context context) {
-        return getPrefs(context).getString(BTC_LINK, DEF_VALUE);
     }
 
     public static int getFileSize(Context context) {
@@ -127,104 +114,100 @@ public class RomUpdate implements Constants {
 
     public static void setRomName(Context context, String name) {
         SharedPreferences.Editor editor = getPrefs(context).edit();
+        String NAME = "rom_name";
         editor.putString(NAME, name);
-        editor.commit();
+        editor.apply();
     }
 
     public static void setVersionName(Context context, String version) {
         SharedPreferences.Editor editor = getPrefs(context).edit();
         editor.putString(VERSION_NAME, version);
-        editor.commit();
+        editor.apply();
     }
 
     public static void setVersionNumber(Context context, int version) {
         SharedPreferences.Editor editor = getPrefs(context).edit();
         editor.putInt(VERSION_NUMBER, version);
-        editor.commit();
+        editor.apply();
     }
 
     public static void setDirectUrl(Context context, String url) {
         SharedPreferences.Editor editor = getPrefs(context).edit();
         editor.putString(DIRECT_URL, url);
-        editor.commit();
+        editor.apply();
     }
 
     public static void setHttpUrl(Context context, String url) {
         SharedPreferences.Editor editor = getPrefs(context).edit();
         editor.putString(HTTP_URL, url);
-        editor.commit();
+        editor.apply();
     }
 
     public static void setMd5(Context context, String md5) {
         SharedPreferences.Editor editor = getPrefs(context).edit();
         editor.putString(MD5, md5);
-        editor.commit();
+        editor.apply();
     }
 
     public static void setChangelog(Context context, String change) {
         SharedPreferences.Editor editor = getPrefs(context).edit();
         editor.putString(CHANGELOG, change);
-        editor.commit();
+        editor.apply();
     }
 
     public static void setAndroidVersion(Context context, String android) {
         SharedPreferences.Editor editor = getPrefs(context).edit();
+        String ANDROID = "rom_android_ver";
         editor.putString(ANDROID, android);
-        editor.commit();
+        editor.apply();
     }
 
     public static void setWebsite(Context context, String website) {
         SharedPreferences.Editor editor = getPrefs(context).edit();
         editor.putString(WEBSITE, website);
-        editor.commit();
+        editor.apply();
     }
 
     public static void setDeveloper(Context context, String developer) {
         SharedPreferences.Editor editor = getPrefs(context).edit();
         editor.putString(DEVELOPER, developer);
-        editor.commit();
+        editor.apply();
     }
 
     public static void setDonateLink(Context context, String donateLink) {
         SharedPreferences.Editor editor = getPrefs(context).edit();
         editor.putString(DONATE_LINK, donateLink);
-        editor.commit();
-    }
-
-    public static void setBitCoinLink(Context context, String donateLink) {
-        SharedPreferences.Editor editor = getPrefs(context).edit();
-        editor.putString(BTC_LINK, donateLink);
-        editor.commit();
+        editor.apply();
     }
 
     public static void setFileSize(Context context, int size) {
         SharedPreferences.Editor editor = getPrefs(context).edit();
         editor.putInt(FILESIZE, size);
-        editor.commit();
+        editor.apply();
     }
 
     public static void setUrlDomain(Context context, String romhut_text) {
         SharedPreferences.Editor editor = getPrefs(context).edit();
         editor.putString(URL_DOMAIN, romhut_text);
-        editor.commit();
+        editor.apply();
     }
 
     public static void setAddonsCount(Context context, int addons_count) {
         SharedPreferences.Editor editor = getPrefs(context).edit();
         editor.putInt(ADDONS_COUNT, addons_count);
-        editor.commit();
+        editor.apply();
     }
 
     public static void setAddonsUrl(Context context, String addons_url) {
         SharedPreferences.Editor editor = getPrefs(context).edit();
         editor.putString(ADDONS_URL, addons_url);
-        editor.commit();
+        editor.apply();
     }
 
     public static void setUpdateAvailable(Context context, boolean availability) {
         SharedPreferences.Editor editor = getPrefs(context).edit();
         editor.putBoolean(AVAILABILITY, availability);
-        editor.commit();
+        editor.apply();
     }
 
     public static String getFilename(Context context) {

app/src/main/java/com/ota/updates/activities/AboutActivity.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2015 Matt Booth (Kryten2k35).
- * Copyright (C) 2017 The halogenOS Project
+ * Copyright (C) 2017 The halogenOS Project.
  *
  * Licensed under the Attribution-NonCommercial-ShareAlike 4.0 International
  * (the "License") you may not use this file except in compliance with the License.
@@ -32,7 +32,6 @@ import com.ota.updates.R;
 import com.ota.updates.adapters.CreditsAdapter;
 import com.ota.updates.tasks.Changelog;
 import com.ota.updates.types.CreditsItem;
-import com.ota.updates.utils.Preferences;
 
 import java.util.ArrayList;
 
@@ -44,7 +43,6 @@ public class AboutActivity extends Activity {
     @SuppressLint("NewApi") @Override
     protected void onCreate(Bundle savedInstanceState) {
         mContext = this;
-        setTheme(Preferences.getTheme(mContext));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ota_about);
 

app/src/main/java/com/ota/updates/activities/AddonActivity.java
@@ -1,22 +1,24 @@
+/*
+ * Copyright (C) 2015 Matt Booth (Kryten2k35).
+ * Copyright (C) 2017 The halogenOS Project.
+ *
+ * Licensed under the Attribution-NonCommercial-ShareAlike 4.0 International
+ * (the "License") you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.ota.updates.activities;
-
-import in.uncod.android.bypass.Bypass;
-
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
-import java.net.URLConnection;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Locale;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.app.ProgressDialog;
 import android.content.Context;
@@ -47,19 +49,31 @@ import com.ota.updates.utils.Constants;
 import com.ota.updates.utils.Preferences;
 import com.ota.updates.utils.Utils;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Locale;
+
+import in.uncod.android.bypass.Bypass;
+
 public class AddonActivity extends Activity implements Constants {
 
     public final static String TAG = "AddonActivity";
 
-    public static Context mContext;
-    private static ListView mListview;
+    public Context mContext;
+    private ListView mListview;
     private static DownloadAddon mDownloadAddon;
 
     @SuppressLint("NewApi") @Override
     public void onCreate(Bundle savedInstanceState) {
         mContext = this;
-        setTheme(Preferences.getTheme(mContext));
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ota_addons);
 
@@ -161,13 +175,13 @@ public class AddonActivity extends Activity implements Constants {
         }
     }
 
-    public static class AddonsArrayAdapter extends ArrayAdapter<Addon> {
+    public class AddonsArrayAdapter extends ArrayAdapter<Addon> {
 
         AddonsArrayAdapter(Context context, ArrayList<Addon> users) {
             super(context, 0, users);
         }
 
-        public static void updateProgress(int index, int progress, boolean finished) {
+        public void updateProgress(int index, int progress, boolean finished) {
             View v = mListview.getChildAt(index -
                     mListview.getFirstVisiblePosition());
 
@@ -187,7 +201,7 @@ public class AddonActivity extends Activity implements Constants {
             }
         }
 
-        public static void updateButtons(int index, boolean finished) {
+        public void updateButtons(int index, boolean finished) {
             View v = mListview.getChildAt((index - 1) -
                     mListview.getFirstVisiblePosition());
 

commit 371223771dc4419850b9038e219d55f5b363b0d5

MainActivity: Fix duplication in info fields
Regression introduced in d1f350e04d28c36d979d436d97c934909a0772f7

Change-Id: If44f2c4d5e41d796a20f74e3f9092184fb2a5ba6

app/src/main/java/com/ota/updates/activities/MainActivity.java
@@ -349,24 +349,24 @@ public class MainActivity extends Activity implements Constants{
         //ROM version
         TextView romVersion = (TextView) findViewById(R.id.tv_main_rom_version);
         String romVersionActual = Utils.getProp(OTA_VERSION);
-        romVersion.setText(String.format("%s%s",romVersion.getText(),romVersionActual));
+        romVersion.setText(String.format("%s", romVersionActual));
 
         //ROM date
         TextView romDate = (TextView) findViewById(R.id.tv_main_rom_date);
         String romDateActual = Utils.getProp("ro.build.date");
-        romDate.setText(String.format("%s%s", romDate.getText(), romDateActual));
+        romDate.setText(String.format("%s", romDateActual));
 
         //ROM android version
         TextView romAndroid = (TextView) findViewById(R.id.tv_main_android_version);
         String romAndroidActual = Utils.getProp("ro.build.version.release");
-        romAndroid.setText(String.format("%s%s",romAndroid.getText(), romAndroidActual));
+        romAndroid.setText(String.format("%s", romAndroidActual));
 
         //ROM developer
         TextView romDeveloper = (TextView) findViewById(R.id.tv_main_rom_developer);
         boolean showDevName = !RomUpdate.getDeveloper(this).equals("null");
         romDeveloper.setVisibility(showDevName? View.VISIBLE : View.GONE);
         String romDeveloperActual = RomUpdate.getDeveloper(this);
-        romDeveloper.setText(String.format("%s%s",romDeveloper.getText(),romDeveloperActual));
+        romDeveloper.setText(String.format("%s", romDeveloperActual));
 
     }
 

build.gradle
@@ -4,7 +4,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:2.3.0'
+        classpath 'com.android.tools.build:gradle:2.3.1'
     }
 }
 